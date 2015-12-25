a9c87dcd7690c0fd7591f99570d305c0e9fb691d
Bas Peters <bas@github.com>
2015-12-25 12:28:36 +0100

    added debug output

--- check-pr.sh
+++ check-pr.sh
@@ -3,6 +3,8 @@ echo "Token: $TOKEN"
 
 curl -u bas:$TOKEN -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "pending","target_url": "https://travis.octodemo.com/office-tools/reading-time-app","description": "Executing PR comment check","context": "pr-check/travis"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/statuses/$TRAVIS_COMMIT
 
+echo "$TRAVIS_PULL_REQUEST"
+
 result=$(curl -u bas:$TOKEN -H "Accept: application/json" -H "Content-type: application/json"  https://octodemo.com/api/v3/repos/office-tools/reading-time-app/pulls/$TRAVIS_PULL_REQUEST | jq '.body')
 
 count=${#result}
