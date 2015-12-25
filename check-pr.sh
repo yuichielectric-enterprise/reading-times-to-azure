94398af48298f66ad2dc8d59b59a0e8afd1d6f6f
Bas Peters <bas@github.com>
2015-12-25 12:32:31 +0100

    copied the calls in the status check

--- check-pr.sh
+++ check-pr.sh
@@ -14,12 +14,10 @@ echo "$count"
 if [ $count -lt 50 ]
 then
   echo "Please add a PR comment of at least 50 characters"
-  CSTATUS="failure"
+  curl -u bas:$TOKEN -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "failure","target_url": "https://travis.octodemo.com/office-tools/reading-time-app","description": "Executing PR comment check","context": "pr-check/travis"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/statuses/$TRAVIS_COMMIT
 else
   echo "Good work! Your PR comment has context!"
-  CSTATUS="success"
+  curl -u bas:$TOKEN -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "success","target_url": "https://travis.octodemo.com/office-tools/reading-time-app","description": "Executing PR comment check","context": "pr-check/travis"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/statuses/$TRAVIS_COMMIT
 fi
 
 echo "PR Code Status: $CSTATUS"
-
-curl -u bas:$TOKEN -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "success","target_url": "https://travis.octodemo.com/office-tools/reading-time-app","description": "Executing PR comment check","context": "pr-check/travis"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/statuses/$TRAVIS_COMMIT
