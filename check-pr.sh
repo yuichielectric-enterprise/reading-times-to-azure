71ebcf193b5e19e79a1da100d1d18d6eb83e64d0
Bas Peters <bas@github.com>
2015-12-25 12:33:52 +0100

    copied the calls in the status check

--- check-pr.sh
+++ check-pr.sh
@@ -1,8 +1,6 @@
 echo "Travis Commit: $TRAVIS_COMMIT"
 echo "Token: $TOKEN"
 
-curl -u bas:$TOKEN -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "pending","target_url": "https://travis.octodemo.com/office-tools/reading-time-app","description": "Executing PR comment check","context": "pr-check/travis"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/statuses/$TRAVIS_COMMIT
-
 echo "$TRAVIS_PULL_REQUEST"
 
 result=$(curl -u bas:$TOKEN -H "Accept: application/json" -H "Content-type: application/json"  https://octodemo.com/api/v3/repos/office-tools/reading-time-app/pulls/$TRAVIS_PULL_REQUEST | jq '.body')
@@ -14,10 +12,12 @@ echo "$count"
 if [ $count -lt 50 ]
 then
   echo "Please add a PR comment of at least 50 characters"
-  curl -u bas:$TOKEN -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "failure","target_url": "https://travis.octodemo.com/office-tools/reading-time-app","description": "Executing PR comment check","context": "pr-check/travis"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/statuses/$TRAVIS_COMMIT
+  CSTATUS="failure"
 else
   echo "Good work! Your PR comment has context!"
-  curl -u bas:$TOKEN -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "success","target_url": "https://travis.octodemo.com/office-tools/reading-time-app","description": "Executing PR comment check","context": "pr-check/travis"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/statuses/$TRAVIS_COMMIT
+  CSTATUS="success"
 fi
 
 echo "PR Code Status: $CSTATUS"
+
+curl -u bas:$TOKEN -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "'"$CSTATUS"'","target_url": "https://travis.octodemo.com/office-tools/reading-time-app","description": "Executing PR comment check","context": "pr-check/travis"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/statuses/$TRAVIS_COMMIT
