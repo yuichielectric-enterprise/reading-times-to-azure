440f29daf3d805ec1c08515658254f2f3efbc673
Bas Peters <bas@github.com>
2015-12-25 08:27:44 +0100

    added firebug to mvn compile and site

--- src/main/java/com/github/demo/launch/Main.java
+++ src/main/java/com/github/demo/launch/Main.java
@@ -14,14 +14,15 @@ public class Main {
         String webappDirLocation = "src/main/webapp/";
         Tomcat tomcat = new Tomcat();
 
-        //The port that we should run on can be set into an environment variable
-        //Look for that variable and default to 8080 if it isn't there.
+        // The port that we should run on can be set into an environment variable
+        // Look for that variable and default to 8080 if it isn't there.
         String webPort = System.getenv("PORT");
         if(webPort == null || webPort.isEmpty()) {
             webPort = "8080";
         }
 
-        tomcat.setPort(Integer.valueOf(webPort));
+
+        tomcat.setPort(Integer.getInteger(webPort));
 
         StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
         System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
