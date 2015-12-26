67774ef97f55c47f2373587a83e6edbe70dc8ce5
Bas Peters <bas@github.com>
2015-12-26 09:49:50 +0100

    removed token echo

--- bogus-status-check.sh
+++ bogus-status-check.sh
@@ -1,7 +1,5 @@
 echo "Travis Commit: $TRAVIS_COMMIT"
-echo "Token: $TOKEN"
-
-echo "$TRAVIS_PULL_REQUEST"
+echo "Travis Pull Request Number: $TRAVIS_PULL_REQUEST"
 
 curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "pending","target_url": "https://travis.octodemo.com/office-tools/reading-time-app","description": "Executing bogus status check","context": "bogus-status-check/travis-ci"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/statuses/$TRAVIS_COMMIT
 
