735e6130128338d48256afef097deab7f414e794
Bas Peters <bas@github.com>
2015-12-28 11:20:49 +0100

    Update BookService.java
    Removed Walden by Henry David Thoreau.

--- src/main/java/com/github/demo/service/BookService.java
+++ src/main/java/com/github/demo/service/BookService.java
@@ -14,7 +14,6 @@ public class BookService {
         books.add(new Book("Kurt Vonnegut","Galápagos"));
         books.add(new Book("Michel Faber","Under the Skin"));
         books.add(new Book("Julian Barnes","A History of the World in 10½ Chapters"));
-        books.add(new Book("Henry David Thoreau","Walden"));
         books.add(new Book("Douglas Adams", "The Hitchhiker's Guide to the Galaxy"));
     }
 
