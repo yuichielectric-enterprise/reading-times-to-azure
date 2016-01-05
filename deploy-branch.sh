264940b7f6c48ec0a642e3c365a7bd0ed8870891
Bas Peters <bas@github.com>
2016-01-05 12:09:33 +0100

    Added environment and echo for travis branch

--- deploy-branch.sh
+++ deploy-branch.sh
@@ -1,6 +1,7 @@
 echo "Travis Commit: $TRAVIS_COMMIT"
+echo "Travis Branch: $TRAVIS_BRANCH"
 
-deployment_id=$(curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"ref": "'"$TRAVIS_COMMIT"'","description": "Deploying branch to neverland"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/deployments | jq '.id')
+deployment_id=$(curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"ref": "'"$TRAVIS_COMMIT"'","description": "Deploying branch to neverland", "environment": "development"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/deployments | jq '.id')
 
 curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "pending","target_url": "https://travis.octodemo.com/office-tools/reading-time-app","description": "Pending deployment to neverland"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/deployments/$deployment_id/statuses
 
