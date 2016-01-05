ee47b20d3172903c9d216b7cf123c0cc62c0459f
Bas Peters <bas@github.com>
2016-01-05 14:42:33 +0100

    fixed url errors caused my variables

--- deploy-branch.sh
+++ deploy-branch.sh
@@ -2,7 +2,9 @@ echo "Travis Commit: $TRAVIS_COMMIT"
 echo "Travis Branch: $TRAVIS_BRANCH"
 echo "Travis repo: $TRAVIS_REPO_SLUG"
 
-deployment_id=$(curl -s -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"ref": "bas-patch-2","description": "Deploying branch to neverland", "environment": "development"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/deployments | jq '.id')
+deployment_id=$(curl -s -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"ref": "'"$TRAVIS_PULL_REQUEST"'","description": "Deploying branch to neverland", "environment": "development"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/deployments | jq '.id')
+
+echo "Deployment ID: $deployment_id"
 
 curl -H "Authorization: Token $TOKEN" -s -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "pending","target_url": "https://travis.octodemo.com/office-tools/reading-time-app","description": "Pending deployment to neverland"}' https://octodemo.com/api/v3/repos/office-tools/reading-time-app/deployments/$deployment_id/statuses
 
