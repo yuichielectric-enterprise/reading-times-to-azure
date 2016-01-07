5363cfbfa020af4a7301c667b4156d2e94cd917d
Bas Peters <bas@github.com>
2016-01-07 14:19:05 +0100

    Update BookService.java

--- src/main/java/com/github/demo/service/BookService.java
+++ src/main/java/com/github/demo/service/BookService.java
@@ -13,8 +13,8 @@ public class BookService {
         books.add(new Book("Philip K. Dick","The Three Stigmata of Palmer Eldritch"));
         books.add(new Book("Kurt Vonnegut","Galápagos"));
         books.add(new Book("Michel Faber","The Book Of Strange New Things"));
-        books.add(new Book("Julian Barnes","A History of the World in 10½ Chapters"));
-        //books.add(new Book("Yuval Noah Harari", "Sapiens: A Brief History of Humankind"));
+        //books.add(new Book("Julian Barnes","A History of the World in 10½ Chapters"));
+        books.add(new Book("Yuval Noah Harari", "Sapiens: A Brief History of Humankind"));
         books.add(new Book("Douglas Adams","The Hitchhiker's Guide to the Galaxy"));
     }
 
