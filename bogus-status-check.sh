413f0268563040c1b8e7f2b1c4a644e8af31b7fa
Bas Peters <bas@github.com>
2016-01-11 15:06:16 +0100

    Update bogus-status-check.sh

--- bogus-status-check.sh
+++ bogus-status-check.sh
@@ -3,6 +3,8 @@ echo "Travis Commit: $TRAVIS_COMMIT"
 echo "Travis Pull Request Number: $TRAVIS_PULL_REQUEST"
 echo "Home directory: $HOME"
 
+ls -l $HOME
+
 curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "pending","target_url": "https://travis.octodemo.com/'$TRAVIS_REPO_SLUG'","description": "Executing bogus status check","context": "bogus-status-check/travis-ci"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/statuses/$TRAVIS_COMMIT
 
 sleep 10
