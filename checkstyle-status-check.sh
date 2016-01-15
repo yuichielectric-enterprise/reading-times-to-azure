90485fc01e057d80bf682ad28f990a0c7b2e80c2
Bas Peters <bas@github.com>
2016-01-15 13:39:27 +0100

    changed script name

--- checkstyle-status-check.sh
+++ checkstyle-status-check.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 echo "Travis Commit: $TRAVIS_COMMIT"
-echo "Travis Pull Request Number: $TRAVIS_PULL_REQUEST"
-echo "Home directory: $HOME"
 
 function statuses () {
   curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "'"$1"'","target_url": "https://travis.octodemo.com/'$TRAVIS_REPO_SLUG'","description": "'"$2"'","context": "checkstyle-status-check/travis-ci"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/statuses/$TRAVIS_COMMIT
 }
 
-statuses "pending" "Checking Java code"
+statuses "pending" "Checking Java code against coding standards"
 
 mvn verify -DskipTests=true
 STATUS=$?
