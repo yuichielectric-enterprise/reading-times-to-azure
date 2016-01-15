bc36567cf3867d332470165db9a5b2fefe6ec7fb
Bas Peters <bas@github.com>
2016-01-15 13:35:16 +0100

    updated ci steps

--- bogus-status-check.sh
+++ bogus-status-check.sh
@@ -4,7 +4,7 @@ echo "Travis Pull Request Number: $TRAVIS_PULL_REQUEST"
 echo "Home directory: $HOME"
 
 function statuses () {
-  curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "'"$1"'","target_url": "https://travis.octodemo.com/'$TRAVIS_REPO_SLUG'","description": "'"$1"'","context": "checkstyle-status-check/travis-ci"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/statuses/$TRAVIS_COMMIT
+  curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "'"$1"'","target_url": "https://travis.octodemo.com/'$TRAVIS_REPO_SLUG'","description": "'"$2"'","context": "checkstyle-status-check/travis-ci"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/statuses/$TRAVIS_COMMIT
 }
 
 statuses "pending" "Checking Java code"
