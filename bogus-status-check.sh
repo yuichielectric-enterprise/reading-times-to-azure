31a10a168aa0211a8631379c47f796831a3f5698
Bas Peters <bas@github.com>
2016-01-15 13:33:58 +0100

    updated ci steps

--- bogus-status-check.sh
+++ bogus-status-check.sh
@@ -4,16 +4,16 @@ echo "Travis Pull Request Number: $TRAVIS_PULL_REQUEST"
 echo "Home directory: $HOME"
 
 function statuses () {
-  curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "'"$1"'","target_url": "https://travis.octodemo.com/'$TRAVIS_REPO_SLUG'","description": "Executing mvn verify with checkstyle","context": "checkstyle-status-check/travis-ci"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/statuses/$TRAVIS_COMMIT
+  curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "'"$1"'","target_url": "https://travis.octodemo.com/'$TRAVIS_REPO_SLUG'","description": "'"$1"'","context": "checkstyle-status-check/travis-ci"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/statuses/$TRAVIS_COMMIT
 }
 
-statuses "pending"
+statuses "pending" "Checking Java code"
 
 mvn verify -DskipTests=true
 STATUS=$?
 echo "$STATUS"
 if [ $STATUS -eq 0 ]; then
-  statuses "success"
+  statuses "success" "Java code meets coding standards"
 else
-  statuses "failure"
+  statuses "failure" "Java code does not meeting coding standards"
 fi
