e9c07a7cb815dd4f05c05eb0838a4ffea45b0f4a
Bas Peters <bas@github.com>
2016-01-15 12:13:43 +0100

    Update bogus-status-check.sh

--- bogus-status-check.sh
+++ bogus-status-check.sh
@@ -5,6 +5,13 @@ echo "Home directory: $HOME"
 
 curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "pending","target_url": "https://travis.octodemo.com/'$TRAVIS_REPO_SLUG'","description": "Executing bogus status check","context": "bogus-status-check/travis-ci"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/statuses/$TRAVIS_COMMIT
 
-sleep 10
+mvn test -DskipTests=true
+STATE="success"
+STATUS=$?
+if [ $STATUS -eq 0 ]; then
+  state="success"
+else
+  state="failure"
+fi
 
-curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "success","target_url": "https://travis.octodemo.com/'$TRAVIS_REPO_SLUG'","description": "Executing bogus status check","context": "bogus-status-check/travis-ci"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/statuses/$TRAVIS_COMMIT
+curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "'"$STATE"'","target_url": "https://travis.octodemo.com/'$TRAVIS_REPO_SLUG'","description": "Executing bogus status check","context": "bogus-status-check/travis-ci"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/statuses/$TRAVIS_COMMIT
