a8cdc60a522dfbee85067f0bd21ecff1c86bacd1
Bas Peters <bas@github.com>
2016-01-15 13:20:40 +0100

    updated ci steps

--- bogus-status-check.sh
+++ bogus-status-check.sh
@@ -4,20 +4,16 @@ echo "Travis Pull Request Number: $TRAVIS_PULL_REQUEST"
 echo "Home directory: $HOME"
 
 function statuses () {
-  curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "'"$STATE"'","target_url": "https://travis.octodemo.com/'$TRAVIS_REPO_SLUG'","description": "Executing mvn verify with checkstyle","context": "checkstyle-status-check/travis-ci"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/statuses/$TRAVIS_COMMIT
+  curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "'"$1"'","target_url": "https://travis.octodemo.com/'$TRAVIS_REPO_SLUG'","description": "Executing mvn verify with checkstyle","context": "checkstyle-status-check/travis-ci"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/statuses/$TRAVIS_COMMIT
 }
 
-STATE="pending"
-
-statuses
+statuses "pending"
 
 mvn verify -DskipTests=true
 STATUS=$?
 echo "$STATUS"
 if [ $STATUS -eq 0 ]; then
-  STATE="success"
+  statuses "success"
 else
-  STATE="failure"
+  statuses "failure"
 fi
-
-statuses
