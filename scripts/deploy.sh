9fefce37ea2e304e7adcf1147878f8fffaf775e1
Bas Peters <bas@github.com>
2016-01-18 11:27:29 -0800

    polished deploy a bit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,7 @@
-echo "Repository: $TRAVIS_REPO_SLUG"
-echo "Token: $TOKEN"
+echo "Owner/Repository: $TRAVIS_REPO_SLUG"
 echo "PR: $TRAVIS_PULL_REQUEST"
 
-function statuses () {
-  curl -s -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "'"$1"'","target_url": "https://reading-time-app.herokuapp.com/","description": "'"$2"'"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/deployments/$deployment_id/statuses
-}
-
 ref=$(curl -s -H "Authorization: Token $TOKEN" -H "Accept: application/json" https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/pulls/${TRAVIS_PULL_REQUEST} | jq '.head.ref')
-
-echo "Pull Request: $ref"
 
 deployment_id=$(curl -s -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"ref": '${ref}',"description": "Deploying branch to test", "environment": "test"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/deployments | jq '.id')
 
@@ -19,9 +12,3 @@ then
   echo "Create deployment failed, please check the branch name: ${ref}"
   exit 1
 fi
-
-#statuses "pending" "Pending deployment to test"
-
-#sleep 10
-
-#statuses "success" "Successfully deployment to test"
