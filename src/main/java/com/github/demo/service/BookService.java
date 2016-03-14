298b90d9bb871063913bcca5b6489c06cf6d1786
Jackie Cooler <bas+jackie@github.com>
2016-03-14 13:02:53 +0100

    Remove Eric Ries
    Remove The Lean Startup by eric ties

--- src/main/java/com/github/demo/service/BookService.java
+++ src/main/java/com/github/demo/service/BookService.java
@@ -11,7 +11,7 @@ public class BookService {
 
     static {
         books.add(new Book("Jeff Sutherland","Scrum: The Art of Doing Twice the Work in Half the Time"));
-        books.add(new Book("Eric Ries","The Lean Startup: How Constant Innovation Creates Radically Successful Businesses"));
+        //books.add(new Book("Eric Ries","The Lean Startup: How Constant Innovation Creates Radically Successful Businesses"));
         books.add(new Book("Geoffrey A. Moore","Crossing the Chasm"));
         books.add(new Book("David Thomas","The Pragmatic Programmer: From Journeyman to Master"));
         books.add(new Book("Frederick P. Brooks Jr.", "The Mythical Man-Month: Essays on Software Engineering"));
