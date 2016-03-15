45bd06e9a6933d8030632ab32fccbadd77d47d34
Bas Peters <bas@github.com>
2016-03-15 19:47:21 +0100

    Add Eric Ries to staff picks
    READ-1 #comment Add The Lean Startup by Eric Ries to staff picks #time 0d 1h #resolve

--- src/main/java/com/github/demo/service/BookService.java
+++ src/main/java/com/github/demo/service/BookService.java
@@ -11,8 +11,8 @@ public class BookService {
 
     static {
         books.add(new Book("Jeff Sutherland","Scrum: The Art of Doing Twice the Work in Half the Time"));
-        //books.add(new Book("Eric Ries","The Lean Startup: How Constant Innovation Creates Radically Successful Businesses"));
-        books.add(new Book("Geoffrey A. Moore","Crossing the Chasm"));
+        books.add(new Book("Eric Ries","The Lean Startup: How Constant Innovation Creates Radically Successful Businesses"));
+        //books.add(new Book("Geoffrey A. Moore","Crossing the Chasm"));
         books.add(new Book("David Thomas","The Pragmatic Programmer: From Journeyman to Master"));
         books.add(new Book("Frederick P. Brooks Jr.", "The Mythical Man-Month: Essays on Software Engineering"));
         books.add(new Book("Steve Krug","Don't Make Me Think, Revisited: A Common Sense Approach to Web Usability"));
