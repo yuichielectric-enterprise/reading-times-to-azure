e13f0be935d4f6ee4e9cdf57549035804b29bb73
Bas Peters <bas@github.com>
2016-06-20 17:55:34 +0100

    updated script to create branch and implement feature

--- scripts/code.sh
+++ scripts/code.sh
@@ -1,2 +1,11 @@
+#!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cp -rf $DIR/resources/* $DIR/../src/ 
+git branch -D add-rating-feature
+git checkout -b add-rating-feature
+cp -rf $DIR/resources/main/java/* $DIR/../src/main/java/
+git add src/main/java/
+git commit -m "Added rating model and service"
+cp -rf $DIR/resources/main/webapp/* $DIR/../src/main/webapp/
+git add src/main/webapp/
+git commit -m "Added rating view"
+git push origin HEAD
