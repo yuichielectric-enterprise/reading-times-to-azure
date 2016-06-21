e2c18f7444002c2a2f41fc824c08cb0f3e2ab749
Vitor Monteiro <bitoiu@github.com>
2016-06-21 10:02:40 +0100

    Deletes all issues, creates new one

--- scripts/reset-demo.sh
+++ scripts/reset-demo.sh
@@ -9,13 +9,31 @@
 # It would be awesome if the bellow used like arrays and data structures, but then again this is bash :(
 
 #!/usr/bin/bash
-echo "Resetting Demo"
+echo "Re-setting Demo"
 
-ISSUE_NUMBER=`curl -H "Authorization: Token $RT_PM_TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"title": "Adding a rating feature to the book store","body": "Body 1"}' https://octodemo.com/api/v3/repos/$RT_ORG/$RT_REPO/issues | jq .number`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function close_issue () {
+  curl -H "Authorization: Token $GITHUB_TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X PATCH -d '{"state": "closed"}' https://octodemo.com/api/v3/repos/$RT_ORG/$RT_REPO/issues/$1
+}
+
+echo "Closing existing issues"
+
+for issue_number in $(curl -s -H "Authorization: Token $GITHUB_TOKEN" -H "Accept: application/json" "https://octodemo.com/api/v3/search/issues?q=state:open+repo:$RT_ORG/$RT_REPO+type:issue&sort=created&order=asc" | jq -r '.items[].number')
+do
+  echo "Closing issue with number: $issue_number"
+  close_issue $issue_number
+done
+
+BODY1=`cat $DIR/issues/message1.md`
+echo $BODY1
+
+ISSUE_NUMBER=`curl -H "Authorization: Token $RT_PM_TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @$DIR/issues/message1.json https://octodemo.com/api/v3/repos/$RT_ORG/$RT_REPO/issues | jq .number`
 
 ECHO "Issue created. Number=$ISSUE_NUMBER"
 
-curl -H "Authorization: Token $RT_UX_TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"body": "Body 2"}' https://octodemo.com/api/v3/repos/$RT_ORG/$RT_REPO/issues/$ISSUE_NUMBER/comments
-curl -H "Authorization: Token $RT_PM_TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"body": "Body 3"}' https://octodemo.com/api/v3/repos/$RT_ORG/$RT_REPO/issues/$ISSUE_NUMBER/comments
-curl -H "Authorization: Token $RT_DS_TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"body": "Body 4"}' https://octodemo.com/api/v3/repos/$RT_ORG/$RT_REPO/issues/$ISSUE_NUMBER/comments
+curl -H "Authorization: Token $RT_UX_TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @$DIR/issues/message2.json https://octodemo.com/api/v3/repos/$RT_ORG/$RT_REPO/issues/$ISSUE_NUMBER/comments
+curl -H "Authorization: Token $RT_PM_TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @$DIR/issues/message3.json https://octodemo.com/api/v3/repos/$RT_ORG/$RT_REPO/issues/$ISSUE_NUMBER/comments
+curl -H "Authorization: Token $RT_DS_TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @$DIR/issues/message4.json https://octodemo.com/api/v3/repos/$RT_ORG/$RT_REPO/issues/$ISSUE_NUMBER/comments
+
 
