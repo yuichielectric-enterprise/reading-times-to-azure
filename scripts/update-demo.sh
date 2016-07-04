0b17b71c8db51a3fe28fbabd6e172545a7133dbc
Vitor Monteiro <bitoiu@github.com>
2016-07-04 14:15:34 +0100

    Updating scripts and travis yml template

--- scripts/update-demo.sh
+++ scripts/update-demo.sh
@@ -3,7 +3,7 @@
 # Fail if no token
 : ${GITHUB_TOKEN?"Please set environment variable GITHUB_TOKEN to the GitHub access token"}
 
-# Gettin the original content
+# Getting the original content
 git remote add baseline git@octodemo.com:baseline/reading-time-demo.git
 git fetch baseline
 
@@ -12,6 +12,7 @@ git checkout master
 git reset --hard baseline/master
 
 # Generating personal travis token
+cp templates/.travis.yml ../
 travis encrypt TOKEN=$GITHUB_TOKEN --add  -e https://travis.octodemo.com/api --debug
 git commit -am "Adding my travis token after demo update"
 
