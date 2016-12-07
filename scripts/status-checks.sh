32c62055ea8f66f33c1a8455e61486fdb1cb9cbc
Johannes Nicolai <jonico@github.com>
2016-12-07 13:13:58 +0100

    Updating references to Travis CI Environment
    As we are moving away from travis.octodemo.com
    to travisci.octodemoapps.com, updating all references.

--- scripts/status-checks.sh
+++ scripts/status-checks.sh
@@ -2,7 +2,7 @@
 echo "Travis Commit: $TRAVIS_COMMIT"
 
 function statuses () {
-  curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "'"$1"'","target_url": "https://travis.octodemo.com/'$TRAVIS_REPO_SLUG'","description": "'"$2"'","context": "'$3'-status-check/travis-ci"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/statuses/$TRAVIS_COMMIT
+  curl -H "Authorization: Token $TOKEN" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state": "'"$1"'","target_url": "https://travisci.octodemoapps.com/'$TRAVIS_REPO_SLUG'","description": "'"$2"'","context": "'$3'-status-check/travis-ci"}' https://octodemo.com/api/v3/repos/${TRAVIS_REPO_SLUG}/statuses/$TRAVIS_COMMIT
 }
 
 if [ "$1" = "verify" ]; then
