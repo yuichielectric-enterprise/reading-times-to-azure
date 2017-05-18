863d72138e0159e6c9811ede2c3cb247ddacaf8e
Lutger <lukeart+octodemo@github.com>
2017-05-18 15:41:00 +0200

    Change $branch -> $BRANCH

--- scripts/deploy-branch.sh
+++ scripts/deploy-branch.sh
@@ -15,7 +15,7 @@ then
   echo "Deployment ID: $deployment_id"
   if (($deployment_id == null))
   then
-    echo "Create deployment failed, please check the branch name '${branch}' and if the last build passed"
+    echo "Create deployment failed, please check the branch name '${BRANCH}' and if the last build passed"
     exit 1
   fi
   statuses "pending" "Pending deployment to test"
