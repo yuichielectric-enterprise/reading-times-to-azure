7b68d6376ad8c6f9b7a5ad629c6e160d9e5c1c23
Yuichi Tanaka <yuichielectric@github.com>
2019-08-02 13:28:26 +0900

    Add Takafumi's book

--- src/main/java/com/github/demo/service/BookService.java
+++ src/main/java/com/github/demo/service/BookService.java
@@ -16,6 +16,7 @@ public class BookService {
         //books.add(new Book("David Thomas","The Pragmatic Programmer: From Journeyman to Master", "pragmatic.jpg"));
         //books.add(new Book("Frederick P. Brooks Jr.", "The Mythical Man-Month: Essays on Software Engineering", "month.jpg"));
         books.add(new Book("Steve Krug","Don't Make Me Think, Revisited: A Common Sense Approach to Web Usability", "think.jpg"));
+        books.add(new Book("Takafumi Ikeda", "Practical collaborative development", "takafumi.jpg"));
     }
 
     public List<Book> getBooks() {
